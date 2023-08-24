973799a1c3232f9cc65ee24b0c57875299e3341e
Sebastián Valencia G <sevalenciagr@gmail.com>
2023-08-24 08:50:45 -0500

    Pruebas en useFetchGifs

--- tests/components/GifGrid.test.jsx
+++ tests/components/GifGrid.test.jsx
@@ -1,5 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import { GifGrid } from "../../src/components/GifGrid";
+import { useFetchGifs } from "../../src/hooks/useFetchGifs";
+
+jest.mock('../../src/hooks/useFetchGifs')
 
 describe('Pruebas en <GifGrid />', () => { 
 
@@ -7,8 +10,38 @@ describe('Pruebas en <GifGrid />', () => {
 
     test('Debe mostrar el loading inicialmente', () => { 
 
+        useFetchGifs.mockReturnValue({
+            images: [],
+            isLoading: true
+        });
+
         render( <GifGrid category = { category } /> );
-        expect( screen.getByText('Loading...') );
+        expect( screen.getByText('Cargando...') );
+        expect( screen.getByText( category ) );
      })
+
+     test('Debe mostrar items cuando se cargan las imagenes useFetchGifs', () => { 
+        
+        const gifs = [
+            {
+                id: 'ABC',
+                title: 'Saitama',
+                url: 'http://localhost/saitama.jpg'
+            },
+            {
+                id: 'XYZ',
+                title: 'Goku',
+                url: 'http://localhost/goku.jpg'
+            },
+        ];    
+        
+        useFetchGifs.mockReturnValue({
+            images: gifs,
+            isLoading: false
+        });
+        render( <GifGrid category={ category } /> );
+        expect( screen.getAllByRole('img').length).toBe( 2 )
+
+      })
 
  })
